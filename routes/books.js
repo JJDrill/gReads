57e258441cd3d0a8549a8db8b41ab1babb0ae643
J. Drill <JJDrill@hotmail.com>
2016-01-18 16:32:38 -0700

    As a user, I need to be able to link to a specific book

--- routes/books.js
+++ routes/books.js
@@ -2,14 +2,23 @@ var express = require('express');
 var router = express.Router();
 var db_Books = require('../db/db_books');
 
-router.get('/list', function(req, res){
+router.get('/:id', function(req, res){
+  if (req.params.id === "new") {
+    res.render('addBooks');
+
+  } else {
+    db_Books.Get_Book(req.params.id).then(function(book){
+      var list = [];
+      list.push(book);
+      res.render('books', { pageData: {bookList : list} });
+    });
+  }
+});
+
+router.get('/', function(req, res){
   db_Books.Get_Book_List().then(function(list){
     res.render('books', { pageData: {bookList : list} });
   });
-});
-
-router.get('/new', function(req, res){
-  res.render('addBooks');
 });
 
 router.post('/new', function(req, res){
@@ -18,14 +27,14 @@ router.post('/new', function(req, res){
                     req.body.coverImageUrl,
                     req.body.description)
   .then(function(id){
-    res.redirect('list');
+    res.redirect('/');
   })
 });
 
 router.get('/delete/:id', function(req, res){
   db_Books.Delete_Book(req.params.id)
   .then(function(){
-    res.redirect('/books/list');
+    res.redirect('/books/');
   })
 });
 
@@ -48,7 +57,7 @@ router.post('/:id/edit', function(req, res){
                     req.body.coverImageUrl,
                     req.body.description)
   .then(function(){
-    res.redirect('/books/list');
+    res.redirect('/books/');
   })
 });
 
