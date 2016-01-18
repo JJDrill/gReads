8e26520f5648c3fca4714a9df9f65e244af55374
J. Drill <JJDrill@hotmail.com>
2016-01-18 13:22:51 -0700

    updated knexfile so PROD works.

--- knexfile.js
+++ knexfile.js
@@ -11,8 +11,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://staposturxkiwo:nPX-GkgL_HIbmRt6r-1I1RrRG8@ec2-54-227-249-166.compute-1.amazonaws.com:5432/ddthgqj582ni44?SSL=true'
-    // connection: process.env.DATABASE_URL + '?SSL=true'
+    connection: 'postgres://staposturxkiwo:nPX-GkgL_HIbmRt6r-1I1RrRG8@ec2-54-227-249-166.compute-1.amazonaws.com:5432/ddthgqj582ni44?ssl=true'
   }
 
 };
